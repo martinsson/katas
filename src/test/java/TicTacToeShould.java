9c0397a91811572bc147ea9d57de4386dfd4bb28
Johan Martinsson <j.martinsson@bee-buzziness.com>
2017-10-20 23:20:28 +0200

    inline all, a bit wierd

--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -117,7 +116,7 @@ public class TicTacToeShould {
 
     private boolean hasWinner(String game, char player) {
         char[] cases = game.toCharArray();
-        Stream<Stream<Integer>> rows = Stream.of(
+        Stream<Stream<Integer>> winningCombinations = Stream.of(
                 // rows
                 Stream.of(0, 1, 2),
                 Stream.of(3, 4, 5),
@@ -130,11 +129,7 @@ public class TicTacToeShould {
                 Stream.of(0, 4, 8),
                 Stream.of(2, 4, 6)
         );
-        return rows.anyMatch(combination -> playerOccupies(player, cases, combination));
-    }
-
-    private boolean playerOccupies(char player, char[] cases, Stream<Integer> combination) {
-        return combination.allMatch(position -> cases[position] == player);
+        return winningCombinations.anyMatch(winningCombination -> winningCombination.allMatch(position -> cases[position] == player));
     }
 
     /*
